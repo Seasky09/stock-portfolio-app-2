339e695ac8d13a591d8fa9e72029991a23d59fa5
Seasky09 <Seasky09@hanmail.net>
2026-06-08 11:54:17 +0900

    Disable table helper scripts to restore loading

--- src/main.jsx
+++ src/main.jsx
@@ -9,9 +9,7 @@ import './premium-dashboard.css'
 import './market-and-isa.css'
 import './excel-table-tools.css'
 import './modal-fix.css'
-import './market-update-helper.js'
 import './compact-metrics.js'
-import './excel-table-tools.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
